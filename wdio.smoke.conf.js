0b5b9d0fa054f937566b0e5e7fc1941514b9f3b1
Phil Segal <phil@phisec.co.uk>
2025-11-03 13:13:48 +0000

    CORE-1602: Add module loader

--- wdio.smoke.conf.js
+++ wdio.smoke.conf.js
@@ -1,6 +1,12 @@
 const debug = process.env.DEBUG
 const oneHour = 60 * 60 * 1000
 
+const execArgv = ['--loader', 'esm-module-alias/loader']
+
+if (debug) {
+  execArgv.push('--inspect')
+}
+
 export const config = {
   //
   // ====================
@@ -19,45 +25,10 @@ export const config = {
   hostname: process.env.CHROMEDRIVER_URL || 'localhost',
   port: process.env.CHROMEDRIVER_PORT || 4444,
 
-  //
-  // ==================
-  // Specify Test Files
-  // ==================
-  // Define which test specs should run. The pattern is relative to the directory
-  // of the configuration file being run.
-  //
-  // The specs are defined as an array of spec files (optionally using wildcards
-  // that will be expanded). The test for each spec file will be run in a separate
-  // worker process. In order to have a group of spec files run in the same worker
-  // process simply enclose them in an array within the specs array.
-  //
-  // If you are calling `wdio` from an NPM script (see https://docs.npmjs.com/cli/run-script),
-  // then the current working directory is where your `package.json` resides, so `wdio`
-  // will be called from there.
-  //
   specs: ['./test/specs/**/*.js'],
-  // Patterns to exclude.
-  exclude: [
-    // 'path/to/excluded/files'
-  ],
-  // injectGlobals: false,
-  //
-  // ============
-  // Capabilities
-  // ============
-  // Define your capabilities here. WebdriverIO can run multiple capabilities at the same
-  // time. Depending on the number of capabilities, WebdriverIO launches several test
-  // sessions. Within your capabilities you can overwrite the spec and exclude options in
-  // order to group specific specs to a specific capability.
-  //
-  // First, you can define how many instances should be started at the same time. Let's
-  // say you have 3 different capabilities (Chrome, Firefox, and Safari) and you have
-  // set maxInstances to 1; wdio will spawn 3 processes. Therefore, if you have 10 spec
-  // files and you set maxInstances to 10, all spec files will get tested at the same time
-  // and 30 processes will get spawned. The property handles how many capabilities
-  // from the same test should run tests.
-  //
-  maxInstances: debug ? 1 : 10,
+  // Tests to exclude
+  exclude: [],
+  maxInstances: 1,
   //
   // If you have trouble getting all important capabilities together, check out the
   // Sauce Labs platform configurator - a great tool to configure your capabilities:
@@ -68,6 +39,7 @@ export const config = {
     {
       maxInstances: 1,
       browserName: 'chrome',
+      'wdio:enforceWebDriverClassic': true,
       'goog:chromeOptions': {
         args: [
           '--no-sandbox',
@@ -80,7 +52,7 @@ export const config = {
     }
   ],
 
-  execArgv: debug ? ['--inspect'] : [],
+  execArgv,
 
   //
   // ===================
